7942e45d21b4f718de19f1e8b16dab0ad92def26
abhilash <abhiwardha@gmail.com>
2020-07-21 15:38:28 +0530

    const data members

---
+++ July21/Day6.2/Day6.2/main.cpp
@@ -0,0 +1,53 @@
+//
+//  main.cpp
+//  Day6.2
+//
+//  Created by ABHILASH  on 21/07/20.
+//  Copyright © 2020 ABHILASH . All rights reserved.
+//
+
+#include <iostream>
+#include<iostream>
+using namespace std;
+  
+//class Test {  //const data members
+//    const int t;
+//
+//public:
+//    Test(int t):t(t) //Initializer
+//    {
+//
+//    }
+//
+//    int getT()
+//    {
+//        return t;
+//
+//    }
+//
+//};
+
+class Test {  //reference members
+    int &t;
+public:
+    Test(int &t):t(t)
+    {
+        
+    }  //Initializer
+    int getT()
+    {
+        return t;
+        
+    }
+};
+int main(int argc, const char * argv[]) {
+//    Test t1(10);
+//    cout<<t1.getT();
+    
+        int x = 20;
+       Test t1(x);
+       cout<<t1.getT()<<endl;
+       x = 30;
+       cout<<t1.getT()<<endl;
+    return 0;
+}
