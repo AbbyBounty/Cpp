2cbad9b65bca73b64d0f43b77af0a2c9d7765e15
abhilash <abhiwardha@gmail.com>
2020-07-27 14:12:43 +0530

    Employee

---
+++ July27/Employee/Employee/main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  Employee
+//
+//  Created by ABHILASH  on 27/07/20.
+//  Copyright © 2020 ABHILASH . All rights reserved.
+//
+
+#include <iostream>
+#include"Salesman.h"
+#include"Manager.h"
+int main(int argc, const char * argv[]) {
+    
+    Manager m1;
+    char name[20] = "Ryan";
+    char add[20] = "NY";
+    Manager m2(1200, 1100, 1, name, 1, 1, 2020, add, 25);
+    cout << "\t*********MANAGER DETAILS************" << endl;
+    m1.display();
+    m2.display();
+    
+    
+    Salesman s1;
+    char nam[20] = "John";
+    char ad[20] = "LA";
+    Salesman s2(1200, 1100, 1, nam, 1, 1, 2020, ad, 25);
+    cout << "\n\n\t*********SALESMAN DETAILS************" << endl;
+    s1.display();
+    s2.display();
+    cout << "\n\t\t";
+    return 0;
+}
