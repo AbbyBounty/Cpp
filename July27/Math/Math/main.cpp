1f685da250be99f85e20015eda9345d02620950f
abhilash <abhiwardha@gmail.com>
2020-07-27 13:38:57 +0530

    Math

---
+++ July27/Math/Math/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Math
+//
+//  Created by ABHILASH  on 27/07/20.
+//  Copyright © 2020 ABHILASH . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
