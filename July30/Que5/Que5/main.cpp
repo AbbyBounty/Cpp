1d5aaef45019063f9d378a8152c3a0462529f8b4
abhilash <abhiwardha@gmail.com>
2020-08-17 22:29:14 +0530

    a

---
+++ July30/Que5/Que5/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  Que5
+//
+//  Created by ABHILASH  on 30/07/20.
+//  Copyright © 2020 ABHILASH . All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+namespace NStudent1
+{
+
+
+}
+
+namespace NStudent2 {
+
+};
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
